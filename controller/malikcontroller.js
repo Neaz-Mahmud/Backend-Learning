8c2ce03285848ff85bc34191f0ea4706c0d1538c
Istiak Hasan <istiakhasan239@gmail.com>
2026-05-18 13:27:12 +0600

    ac

--- controller/malikcontroller.js
+++ controller/malikcontroller.js
@@ -1,4 +1,7 @@
 const { homedata } = require("../model/malikdata");
+const fs = require('fs');
+const path = require('path');
+
 
 const getaddhome = (req, res, next) => {
   res.render("add-home");
@@ -7,9 +10,18 @@ const getaddhome = (req, res, next) => {
 const homelist = [];
 
 const postaddhome = (req, res, next) => {
-  console.log("come to post data");
+
   const b = new homedata(req.body);
   b.save();
+
+  const homedatastore = path.join(
+    path.dirname(require.main.filename), 'data','home.json'
+  );
+
+  fs.writeFile(homedatastore, JSON.stringify(homedata.fetchall()),(errr)=>{
+   console.log("error happens");
+  });
+
 
   res.render("successfull");
 };
